8796b2f469536c9435f3dd42c5be4c371c100512
Alouiscious Media <info@reneesoul.com>
2019-10-20 18:06:27 -0700

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+  SHIPS = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:typw]
+    @height = params[:booty]
+
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
+
 end
